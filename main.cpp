f786f715beae6e6862af4ce70529019daff87881
KubInDev <54432043+KubInDev@users.noreply.github.com>
2019-12-16 20:14:36 +0100

    Main
    dada

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+int liczba[7];
+
+void wpis() {
+    cout << "Wpisz swoje liczby po Enterze" << endl;
+    for(int i=0; i<8; i++){
+        cin >> liczba[i];
+    }
+}
+
+void wypis() {
+    cout << "Oto twoje liczby: ";
+    for(int x=0; x<8; x++) {
+        cout << liczba[x] << ",";
+    }
+}
+
+void sortowanie() {
+    int i, j,temp,passes=0;
+    cout<<endl;
+    for(i = 0; i<10; i++) {
+        for(j = i+1; j<8; j++)
+        {
+            if(liczba[j] < liczba[i]) {
+                temp = liczba[i];
+                liczba[i] = liczba[j];
+                liczba[j] = temp;
+            }
+        }
+        passes++;
+    }
+    cout <<"Wysortowane elementy: \n";
+    for(i = 0; i<8; i++) {
+        cout <<liczba[i]<<"\t";
+    }
+    cout<<"\nIlosc sortowan:"<<passes<<endl;
+}
+
+main(){
+    //Wpis i wypis liczb
+    wpis();
+    wypis();
+    ///////////////////////
+
+    //Sortowanie
+    sortowanie();
+
+}
